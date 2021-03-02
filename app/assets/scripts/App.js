2db030d52fd06c7f0b7d0c301e5949b3c5453f01
Majed <webdev.majed@gmail.com>
2021-03-02 20:36:06 +0200

    Completed the Modal scripts and styles.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,10 +3,32 @@ import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll'
 import StickyHeader from './modules/StickyHeader'
 
-let stickyHeader = new StickyHeader()
-let mobileMenu = new MobileMenu()
+/* see the extension called event-emitter 
+The EventEmitter is a module that facilitates communication/interaction between objects in Node*/
+new StickyHeader()
+new MobileMenu()
 new RevealOnScroll(document.querySelectorAll('.feature-item'), 75)
 new RevealOnScroll(document.querySelectorAll('.testimonial'), 60)
+let modal
+
+/* Load/ import the modal script only when the visitor click on open button */
+document.querySelectorAll('.open-modal').forEach(el => {
+  el.addEventListener('click', e => {
+    if (typeof modal == 'undefined') {
+      e.preventDefault()
+      import(/* webpackChunkName: "modal" */ './modules/Modal')
+        .then(x => {
+          modal = new x.default()
+          setTimeout(() => modal.openModal(), 20)
+        })
+        .catch(() =>
+          console.log('There was a problem while loading the asset!')
+        )
+    } else {
+      modal.openModal()
+    }
+  })
+})
 
 if (module.hot) {
   module.hot.accept()
